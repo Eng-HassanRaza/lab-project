170e633761428a59f7cda08f46337af9cbef5ba2
hassan-raza <hassan.numl2013@gmail.com>
2021-05-03 00:15:22 +0500

    server domin update

--- core/settings.py
+++ core/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 # load production server from .env
-ALLOWED_HOSTS = ['localhost','52.87.23.180' ,'127.0.0.1', config('SERVER', default='127.0.0.1')]
+ALLOWED_HOSTS = ['localhost','52.87.23.180','analyticks.com.pk' ,'www.analyticks.com.pk' ,'analyticks.pk' ,'www.analyticks.pk' ,'127.0.0.1', config('SERVER', default='127.0.0.1')]
 
 # Application definition
 
